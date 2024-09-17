1b20e45411a6384be97110c77337dee72bbe9bd5
MlgEpicCar <115513939+MlgEpicCar@users.noreply.github.com>
2024-09-17 14:48:47 -0400

    wait... im coded?

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById("h1").textContent = `Favorite Foods`;
+
+document.getElementById("li0").textContent = `Chicken`;
+document.getElementById("li1").textContent = `Pizza`;
+document.getElementById("li2").textContent = `Waffle`;
+document.getElementById("li3").textContent = `Taco`;
+document.getElementById("li4").textContent = `Burger`;
+
+console.log("JavaScript is pretty cool, unfortunatly our school computers don't let us use inspect element so i'm not sure how to see if what i'm writing here will even appear in the correct spot");
